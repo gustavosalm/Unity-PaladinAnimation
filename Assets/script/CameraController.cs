87107b3b0917fb71b965fe7a32370d61f4885f21
gustavosalm <gustavosalmeida9821@gmail.com>
2020-12-16 21:42:35 -0300

    projeto organizado e umas finalizaçoes

---
+++ Assets/script/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float mx, my, sensitivity = 50, xRot = 0;
+    private GameObject pl;
+    void Start()
+    {
+        pl = GameObject.FindWithTag("Player");
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        mx = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        //my = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        // xRot -= my;
+        // xRot = Mathf.Clamp(xRot, -90, 90);
+        // transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+
+        pl.transform.Rotate(Vector3.up * mx);
+    }
+}
